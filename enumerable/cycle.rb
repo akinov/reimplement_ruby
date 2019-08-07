79364d2a5a0afd3b8c78385d4b50fab8ccd55bea
akinov <ayumoto10@gmail.com>
2019-08-07 22:03:11 +0900

    Reimplement cycle

--- enumerable/cycle.rb
+++ enumerable/cycle.rb
@@ -1,5 +1,19 @@
 class Array
   def cycle(n = nil)
-    puts 'reimprement here'
+    return to_enum(:cycle, n) if n&.negative?
+
+    enumerator = Enumerator.new do |y|
+      counter = 0
+      while n != counter
+        each { |v| y << v }
+        counter += 1
+      end
+    end
+
+    if block_given?
+      enumerator.each { |v| yield v }
+    else
+      enumerator
+    end
   end
 end
