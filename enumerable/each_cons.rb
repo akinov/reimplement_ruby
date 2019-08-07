0013dfcfb3349d72dd63f4b8cb1cc8b2b120132b
akinov <ayumoto10@gmail.com>
2019-08-07 14:36:34 +0900

    Add Enumerable#each_cons

---
+++ enumerable/each_cons.rb
@@ -0,0 +1,5 @@
+module Enumerable
+  def each_cons(n, &block)
+    puts 'reimprement here'
+  end
+end
---
+++ enumerable/each_cons_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require_relative 'each_cons'
+
+class EachConsTest < MiniTest::Test
+  def test_each_cons
+    each_cons_result = (1..5).each_cons(3)
+    
+    assert_equal [[1, 2, 3],[2, 3, 4],[3, 4, 5]], each_cons_result.map {|v| v }
+    assert_equal Enumerator, each_cons_result.class
+  end
+
+  def test_each_cons_block
+    assert_output("[1, 2, 3]\n[2, 3, 4]\n[3, 4, 5]\n") { (1..5).each_cons(3) {|v| p v } }
+  end
+end
