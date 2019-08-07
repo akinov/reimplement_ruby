7d8b6c4c5436eaf6f3e7ff092f7337b3ffa2a191
akinov <ayumoto10@gmail.com>
2019-08-07 15:05:37 +0900

    Add Enumerable#cycle

---
+++ enumerable/cycle.rb
@@ -0,0 +1,5 @@
+class Array
+  def cycle(n = nil, &block)
+    puts 'reimprement here'
+  end
+end
---
+++ enumerable/cycle_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require_relative 'cycle'
+
+class CycleTest < MiniTest::Test
+  def test_cycle
+    cycle_result = %w(a b c).cycle(2)
+    
+    assert_equal %w(a b c a b c), cycle_result.map {|v| v }
+    assert_equal Enumerator, cycle_result.class
+  end
+
+  def test_cycle_negative_args
+    assert_output("") { %w(a b c).cycle(-1) { |v| p v } }
+  end
+end
