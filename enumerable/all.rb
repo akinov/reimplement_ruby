60aa2d84ef60f5b3fd34b4ffb4e75278e28b0cec
akinov <ayumoto10@gmail.com>
2019-08-07 22:17:24 +0900

    Add Enumerable#all?

---
+++ enumerable/all.rb
@@ -0,0 +1,5 @@
+module Enumerable
+  def all?
+    puts 'reimprement here'
+  end
+end
---
+++ enumerable/all_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require_relative 'all'
+
+class AllTest < MiniTest::Test
+  # all? -> bool
+  def test_all
+    assert_equal true, [true, true].all?
+    assert_equal false, [true, false].all?
+  end
+
+  # all? {|item| ... } -> bool
+  def test_all_block_given
+    assert_equal true, [5,  6, 7].all? {|v| v > 0 }
+    assert_equal false, [5, -1, 7].all? {|v| v > 0 }
+    assert_equal true, [].all? {|v| v > 0 } 
+  end
+
+  # all?(pattern) -> bool
+  def test_all_pattern
+    assert_equal false, %w[ant bear cat].all?(/t/)
+  end
+end
