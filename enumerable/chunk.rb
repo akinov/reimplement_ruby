a3f12cd486555187897a67e1bdec7f063344bd18
akinov <ayumoto10@gmail.com>
2019-08-07 22:03:03 +0900

    Reimplement chunk

--- enumerable/chunk.rb
+++ enumerable/chunk.rb
@@ -1,5 +1,22 @@
 module Enumerable
   def chunk
-    puts 'reimprement here'
+    return to_enum(:chunk) unless block_given?
+    
+    Enumerator.new do |y|
+      tmp = []
+      block_result = nil
+      each do |n|
+        tmp_block_result = yield(n)
+        if !tmp.empty? && block_result != tmp_block_result
+          y << [block_result, tmp]
+          tmp = []
+          block_result = tmp_block_result
+        end
+        block_result = tmp_block_result
+        tmp << n
+
+      end
+      y << [block_result, tmp]
+    end
   end
 end
