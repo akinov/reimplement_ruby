1f0ce01bf460f107640cbb121757579b9a4863b7
akinov <ayumoto10@gmail.com>
2019-08-08 19:39:40 +0900

    Add Enumerable#any?

---
+++ enumerable/any.rb
@@ -0,0 +1,5 @@
+class Array
+  def any?(pattern = nil)
+    puts 'reimprement here'
+  end
+end
---
+++ enumerable/any_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require_relative 'any'
+
+class AnyTest < MiniTest::Test
+  # any? -> bool
+  def test
+    assert_equal true, [nil, true, 99].any?
+    assert_equal false, [].any?
+  end
+
+  # any? {|item| ... } -> bool
+  def test_block_given
+    assert_equal false, [1, 2, 3].any? { |v| v > 3 }
+    assert_equal true, [1, 2, 3].any? { |v| v > 1 }
+    assert_equal false, [].any?(&:positive?)
+  end
+
+  # any?(pattern) -> bool
+  def test_pattern
+    assert_equal false, %w[ant bear cat].any?(/d/)
+    assert_equal true, [nil, true, 99].any?(Integer)
+  end
+end
