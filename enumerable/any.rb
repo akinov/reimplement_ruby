3391ef109694e9de3ba711aea1eb55701b0157dd
akinov <ayumoto10@gmail.com>
2019-08-09 23:36:08 +0900

    Reimprement any?

--- enumerable/any.rb
+++ enumerable/any.rb
@@ -1,5 +1,29 @@
 class Array
   def any?(*args)
-    puts 'reimprement here'
+    case args.length
+    when 0
+      lamda = if block_given?
+                ->(v) { yield v }
+              else
+                ->(v) { v }
+              end
+    when 1
+      pettern = args.shift
+      lamda = if pettern.is_a? Class
+                ->(v) { v.is_a? pettern }
+              elsif pettern.is_a? Regexp
+                ->(v) { v =~ pettern }
+              else
+                ->(v) { v === pettern }
+              end
+    else
+      raise ArgumentError, "wrong number of arguments (given #{args.length}, expected 0..1)"
+    end
+
+    each do |n|
+      return true if lamda.call(n)
+    end
+
+    false
   end
 end
