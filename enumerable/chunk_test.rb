5bc4b90ad4aeed7a67e8be3358c156c330284198
akinov <ayumoto10@gmail.com>
2021-08-15 13:22:04 +0900

    add chunk test

--- enumerable/chunk_test.rb
+++ enumerable/chunk_test.rb
@@ -10,4 +10,20 @@ class ChunkTest < MiniTest::Test
     assert_equal expected_result, chunk_result.map { |even, ary| [even, ary] }
     assert_equal Enumerator, chunk_result.class
   end
+
+  def test_chunk_separator
+    chunk_result = [2, 4, 5, 7, 4, 5, 6, 8, 9, 7, 1].chunk { |n| (n < 5) ? :_separator : n.even? }
+    expected_result = [[false, [5, 7]], [false, [5]], [true, [6, 8]], [false, [9, 7]]]
+
+    assert_equal expected_result, chunk_result.map { |even, ary| [even, ary] }
+    assert_equal Enumerator, chunk_result.class
+  end
+
+  def test_chunk_alone
+    chunk_result = [2, 4, 5, 7, 4, 4, 2, 2, 9, 7, 1].chunk { |n| (n == 2) ? :_alone: n.even? }
+    expected_result = [[:_alone, [2]], [true, [4]], [false, [5, 7]], [true, [4, 4]], [:_alone, [2]], [:_alone, [2]], [false, [9, 7, 1]]]
+
+    assert_equal expected_result, chunk_result.map { |even, ary| [even, ary] }
+    assert_equal Enumerator, chunk_result.class
+  end
 end
